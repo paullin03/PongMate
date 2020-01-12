a6f978ef89d0474cd0bb7ac3577c0e1fb519df13
Harman Brar <rphenterprisesltd@gmail.com>
2020-01-11 16:47:28 -0800

    added foundation for init screen

---
+++ app/src/main/java/com/example/android/pongmate/InitActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.pongmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_init);
+    }
+}
